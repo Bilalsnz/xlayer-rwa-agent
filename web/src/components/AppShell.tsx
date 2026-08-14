4c323974ce07a5b1991af5e10bb8e0bfcbc2e74e
Bilalsnz <160939829+Bilalsnz@users.noreply.github.com>
2026-08-14 09:21:18 +0100

    Update AppShell.tsx
    fix: stronger doAnchor with better chain switch

--- web/src/components/AppShell.tsx
+++ web/src/components/AppShell.tsx
@@ -1,4 +1,4 @@
-"use client";
+ "use client";
 
 import { useEffect, useRef, useState } from "react";
 import { useAccount, useChainId, useWriteContract, useSendTransaction, useSwitchChain } from "wagmi";
